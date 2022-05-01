28fc0eaa7fa6bdc1b45071420eec54b54c2da6d0
nekoimi <nekoimime@gmail.com>
2022-05-01 21:26:06 +0800

    :construction: update

--- main.js
+++ main.js
@@ -2,10 +2,28 @@ const net = require("net")
 
 // 创建server
 const server = net.createServer()
-server.on("listening", function () {
-    console.log("服务启动监听...")
-})
-// 接受新的客户端链接
-server.on("connection", function () {
 
+// 记录日志
+server.on("listening", function () {
+    console.info("服务启动监听...")
 })
+
+// 处理错误
+server.on("error", function (error) {
+    console.error(error)
+})
+
+// 接受新的客户端链接
+server.on("connection", function (socket) {
+    console.info("客户端连接: ", socket.address())
+    socket.on("data", function () {
+
+    })
+})
+
+// 关闭
+server.on("close", function () {
+    console.info("关闭连接")
+})
+
+server.listen(8080, "0.0.0.0")
